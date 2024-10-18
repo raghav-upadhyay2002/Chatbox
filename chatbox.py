5beb80931f7be2757535f527f05461274a7fc0d5
Raghav Upadhyay <ru7440@srmist.edu.in>
2024-10-18 01:49:05 -0700

    Chatbox file updated

--- chatbox.py
+++ chatbox.py
@@ -1,5 +1,14 @@
 import pandas as pd
 import openai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+# Now use the API key securely in your code
 
 
-openai.api_key="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"
+
+
+chat_box=domains
